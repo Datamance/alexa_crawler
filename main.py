aa703f620f697ac29e0c02b306fe3680cbe12955
Rico Rodriguez <rico.l.rodriguez@gmail.com>
2018-08-24 09:40:49 -0400

    Looks like these are the best numbers I can squeeze

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ import zipfile
 from collections import Counter
 from pprint import pprint
 from urllib.parse import urlparse
+import functools
+import time
 
 import aiohttp
 import requests
@@ -25,26 +27,26 @@ from lxml import etree
 
 
 TWO_HUNDRED_THOUSAND = 200 * 1000
-DEFAULT_BATCH = TWO_HUNDRED_THOUSAND // 1000  # 1000 ClientSessions, 200 each.
 ALEXA_TOP_MILLION_URL = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
 DEFAULT_CSV = "top_200K_with_favicon_urls.csv"
 
-HTML_PARSER = etree.HTMLParser()
+# HTML_PARSER = etree.HTMLParser()
 
 # løøp, bröther
 EVENT_LOOP = asyncio.get_event_loop()
+TEN_MINUTE_TIMEOUT = aiohttp.ClientTimeout(600)
+ONE_MINUTE_TIMEOUT = aiohttp.ClientTimeout(60)
 THIRTY_SECONDS_TIMEOUT = aiohttp.ClientTimeout(30)
 TEN_SECONDS_TIMEOUT = aiohttp.ClientTimeout(10)
 
 # # Set up Queues
-# FAVICON_GETTER_QUEUE = asyncio.Queue(loop=EVENT_LOOP)
-# CSV_WRITER_QUEUE = asyncio.Queue(loop=EVENT_LOOP)
+CSV_WRITER_QUEUE = asyncio.Queue(loop=EVENT_LOOP)
 
 
-ERROR_COUNTER = Counter()
+counter = Counter()
 
 
-def get_ranks_and_domains(total_number, batch_size):
+def get_ranks_and_domains(total_number):
     """Synchronously prepare the data."""
     # Consideration - if this were a much larger file, we might opt to stream
     # instead and use iter_content() and a zlib decompressor to iteratively
@@ -53,19 +55,11 @@ def get_ranks_and_domains(total_number, batch_size):
     content = requests.get(ALEXA_TOP_MILLION_URL).content
 
     with zipfile.ZipFile(io.BytesIO(content), "r") as zipped:
-        print("unpacking bytes...")
+        print("unpacking zipfile...")
         text_as_bytes = zipped.read("top-1m.csv")
 
     pairs = text_as_bytes.split(b"\n")[:total_number]
-
-    chunks = []
-
-    while pairs:
-        chunks.append(
-            [pairs.pop().decode("utf-8").split(",") for _ in range(batch_size)]
-        )
-
-    return chunks
+    return [pair.decode("utf-8").split(",") for pair in pairs]
 
 
 def get_favicon(html, url):
@@ -116,108 +110,126 @@ def get_favicon(html, url):
     return None
 
 
-async def _fetch_html(session, url):
+async def _fetch_html(semaphore, session, url):
     """Helper method to fetch the html content."""
-    async with session.get(
-        url, timeout=TEN_SECONDS_TIMEOUT, ssl=False
-    ) as response:
-        if response.status == 200:
-            return await response.read()
-        else:
-            ERROR_COUNTER.update(["NON-200 RESPONSE"])
-            return None
+    async with semaphore:
+        async with session.get(
+            url, timeout=TEN_MINUTE_TIMEOUT, ssl=False
+        ) as response:
+            if response.status == 200:
+                return await response.read()
+            else:
+                return None
 
 
-async def _try_get_favicon(session, url):
+async def bound_fetch(semaphore, url, session):
+    # Getter function with semaphore.
+    async with semaphore:
+        return await _fetch_html(session, url)
+
+
+async def _try_get_favicon(semaphore, session, url):
     """Blind attempt at getting the favicon."""
     maybe_favicon = url.rstrip("/") + "/favicon.ico"
-    async with session.get(
-        maybe_favicon, timeout=TEN_SECONDS_TIMEOUT, ssl=False
-    ) as response:
-        if response.status == 200:
-            body_bytes = await response.content.read()
-            # Is there actually something here?
-            return maybe_favicon if len(body_bytes) > 0 else None
+    async with semaphore:
+        async with session.get(
+            maybe_favicon, timeout=TEN_SECONDS_TIMEOUT, ssl=False
+        ) as response:
+            if response.status == 200:
+                body_bytes = await response.content.read()
+                # Is there actually something here?
+                return maybe_favicon if len(body_bytes) > 0 else None
+            else:
+                return None
+
+
+async def write_to_csv(csv_writer):
+    """Writes to the csv"""
+    while True:
+        row = await CSV_WRITER_QUEUE.get()
+        if row is None:
+            break
         else:
-            return None
+            counter["success"] += 1
+            total = counter['success'] + counter['fail']
+            percent = (counter['success'] / total) * 100
+            print(f"pcnt: {percent:.2f}, total: {total}")
+            pprint(counter.most_common())
+            csv_writer.writerow(row)
 
 
-async def get_row(session, rank, domain):
+async def get_row(semaphore, session, rank, domain):
     url = f"http://www.{domain}"
     favicon_url = None
 
     # First, make a blind attempt.
     try:
-        favicon_url = await _try_get_favicon(session, url)
+        favicon_url = await _try_get_favicon(semaphore, session, url)
         if favicon_url:
-            return (url, rank, favicon_url)
+            await CSV_WRITER_QUEUE.put((url, rank, favicon_url))
     except Exception as e:
         pass  # We don't really care.
 
     # Otherwise, we have to dive into the HTML.
     try:
-        text_content = await _fetch_html(session, url)
+        text_content = await _fetch_html(semaphore, session, url)
     except Exception as e:
-        ERROR_COUNTER.update([type(e)])
+        counter[type(e)] += 1
+        counter["fail"] += 1
         return None
 
     if not text_content:
-        ERROR_COUNTER.update(["NO TEXT CONTENT"])
+        counter["no text"] += 1
+        counter["fail"] += 1
         return None
 
     favicon_url = get_favicon(text_content, url)
 
     if not favicon_url:
-        ERROR_COUNTER.update(["NO FAVICON URL"])
+        counter["no favicon"] += 1
+        counter["fail"] += 1
         return None
 
-    return (url, rank, favicon_url)
+    await CSV_WRITER_QUEUE.put((url, rank, favicon_url))
 
 
-async def get_all(chunks_of_work, csv_writer):
+async def run(pairs):
     """Main doer of work."""
-    all_results = []
+    # all_results = []
+    semaphore = asyncio.Semaphore(1000)
+    one_thousand = aiohttp.TCPConnector(limit=300)
 
-    async with aiohttp.ClientSession(loop=EVENT_LOOP) as session:
-        for rank_domain_pairs in chunks_of_work:
-            rows = await asyncio.gather(
-                *[get_row(session, *pair) for pair in rank_domain_pairs]
-            )
+    async with aiohttp.ClientSession(
+            loop=EVENT_LOOP, connector=one_thousand) as session:
+        rows = await asyncio.gather(
+            *[get_row(semaphore, session, *pair) for pair in pairs]
+        )
 
-            success_batch = [row for row in rows if row is not None]
+        successes = [row for row in rows if row is not None]
 
-            pprint(
-                {
-                    "Failures: ": ERROR_COUNTER.most_common(),
-                    "Successes: ": len(success_batch),
-                }
-            )
+    await CSV_WRITER_QUEUE.put(None)
 
-            csv_writer.writerows(success_batch)
-
-            all_results.extend(success_batch)
-
-    return all_results
+    return 0
 
 
 def main(
     csv_file_path=DEFAULT_CSV,
-    total_number=TWO_HUNDRED_THOUSAND,
-    batch_size=DEFAULT_BATCH,
+    total_number=TWO_HUNDRED_THOUSAND
 ):
     """Main execution context, controls event loop."""
     # 1) Set up CSV file.
-    csv_file = open(csv_file_path, "w", batch_size)
+    csv_file = open(csv_file_path, "w", 1000)
     csv_writer = csv.writer(csv_file)
     csv_writer.writerow(("domain", "rank", "favicon_url"))
 
     # 2) Get work chunked up,
-    chunks_of_work = get_ranks_and_domains(total_number, batch_size)
+    pairs = get_ranks_and_domains(total_number)
 
     try:
-        EVENT_LOOP.run_until_complete(get_all(chunks_of_work, csv_writer))
+        EVENT_LOOP.create_task(write_to_csv(csv_writer))
+        EVENT_LOOP.run_until_complete(run(pairs))
     except Exception as e:  # this is pretty gross but whatever
-        print(f"FATAL ERROR")
+        print(f"******FATAL ERROR********")
         asyncio.gather(*asyncio.Task.all_tasks()).cancel()
     finally:
         # Shutting down and closing file descriptors after interrupt
