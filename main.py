e17ffbf4c35dc16c894c6ad3be6b88b418b6a51c
Rico Rodriguez <rico.l.rodriguez@gmail.com>
2018-08-24 15:05:52 -0400

    cleaning up

--- main.py
+++ main.py
@@ -10,40 +10,43 @@ __author__ = "Rico Rodriguez"
 
 import asyncio
 import csv
-import functools
 import io
+import logging
 import os
 import time
 import zipfile
 from collections import Counter
-from pprint import pprint
+from logging.config import fileConfig
+from typing import Iterable, Tuple
 from urllib.parse import urlparse
 
 import aiohttp
 import requests
 from aiohttp.client_exceptions import ClientConnectorError
-from bs4 import BeautifulSoup
 from lxml import etree
 
 
+# Set up Logging.
+fileConfig("logging_config.ini")
+logger = logging.getLogger()
+
+# Constants.
 TWO_HUNDRED_THOUSAND = 200 * 1000
 ALEXA_TOP_MILLION_URL = "http://s3.amazonaws.com/alexa-static/top-1m.csv.zip"
 DEFAULT_CSV = "top_200K_with_favicon_urls.csv"
 
-# HTML_PARSER = etree.HTMLParser()
+# Initialize now so we don't have to over and over again.
+HTML_PARSER = etree.HTMLParser()
 
-# løøp, bröther
+# Løøp, bröther.
 EVENT_LOOP = asyncio.get_event_loop()
 TEN_MINUTE_TIMEOUT = aiohttp.ClientTimeout(600)
 ONE_MINUTE_TIMEOUT = aiohttp.ClientTimeout(60)
 THIRTY_SECONDS_TIMEOUT = aiohttp.ClientTimeout(30)
 TEN_SECONDS_TIMEOUT = aiohttp.ClientTimeout(10)
 
-# # Set up Queues
+# # Set up Queues.
 CSV_WRITER_QUEUE = asyncio.Queue(loop=EVENT_LOOP)
-
-
-counter = Counter()
 
 
 def get_ranks_and_domains(total_number):
@@ -65,17 +68,25 @@ def get_ranks_and_domains(total_number):
 def get_favicon(html, url):
     """Slightly modified version of pyfav's get_favicon_from_markup method.
 
+    Forgoing BS4 cuz it's too heavyweight.
+
     https://github.com/phillipsm/pyfav/blob/master/pyfav/pyfav.py
     """
     parsed_site_uri = urlparse(url)
 
-    soup = BeautifulSoup(html, features="lxml")
+    tree = etree.fromstring(html, parser=HTML_PARSER)
 
     # Do we have a link element with the icon?
-    icon_link = soup.find("link", rel="icon")
-    if icon_link and icon_link.has_attr("href"):
+    possible_favicons = []
+    for link in tree.cssselect("link"):
+        if "icon" in link.attrib.get("rel", "").lower() and link.attrib.get(
+            "href"
+        ):
+            possible_favicons.append(link)
 
-        favicon_url = icon_link["href"]
+    if possible_favicons:
+
+        favicon_url = possible_favicons.pop().attrib["href"]
 
         # Sometimes we get a protocol-relative path
         if favicon_url.startswith("//"):
@@ -110,8 +121,19 @@ def get_favicon(html, url):
     return None
 
 
-async def _fetch_html(semaphore, session, url):
-    """Helper method to fetch the html content."""
+async def _fetch_html(
+    semaphore: asyncio.Semaphore, session: aiohttp.ClientSession, url: str
+):
+    """Helper coroutine to fetch the html content.
+
+    Args:
+        semaphore: asyncio.Semaphore. Lock used to control connection pool.
+        session: aiohttp.ClientSession. Session maintaining TCPConnector.
+        url: String. the URL we're trying to fetch.
+
+    Returns:
+        bytes or None.
+    """
     async with semaphore:
         async with session.get(
             url, timeout=TEN_MINUTE_TIMEOUT, ssl=False
@@ -122,18 +144,26 @@ async def _fetch_html(semaphore, session, url):
                 return None
 
 
-async def bound_fetch(semaphore, url, session):
-    # Getter function with semaphore.
-    async with semaphore:
-        return await _fetch_html(session, url)
+async def _try_get_favicon(
+    semaphore: asyncio.Semaphore, session: aiohttp.ClientSession, url: str
+):
+    """Blind attempt at getting the favicon.
 
+    Args:
+        semaphore: asyncio.Semaphore. Lock used to control connection pool.
+        session: aiohttp.ClientSession. Session maintaining TCPConnector.
+        url: String. The favicon URL we're blindly reaching out for.
 
-async def _try_get_favicon(semaphore, session, url):
-    """Blind attempt at getting the favicon."""
+    Returns:
+        str or None.
+    """
     maybe_favicon = url.rstrip("/") + "/favicon.ico"
     async with semaphore:
         async with session.get(
-            maybe_favicon, timeout=TEN_SECONDS_TIMEOUT, ssl=False
+            # Short timeout because the favicon payload should be small.
+            maybe_favicon,
+            timeout=TEN_SECONDS_TIMEOUT,
+            ssl=False,
         ) as response:
             if response.status == 200:
                 body_bytes = await response.content.read()
@@ -144,77 +174,115 @@ async def _try_get_favicon(semaphore, session, url):
 
 
 async def write_to_csv(csv_writer):
-    """Writes to the csv"""
+    """Chained coroutine, writes rows from out of queue.
+
+    Args:
+        csv_writer: Object retrieved from csv.writer(). Writes to the CSV.
+
+    Side Effects:
+        Writes a completed row to a CSV.
+    """
     while True:
         row = await CSV_WRITER_QUEUE.get()
         if row is None:
             break
         else:
-            counter["success"] += 1
-            total = counter["success"] + counter["fail"]
-            percent = (counter["success"] / total) * 100
-            print(f"pcnt: {percent:.2f}, total: {total}")
-            pprint(counter.most_common())
+            logger.info(f"Success for {row}")
             csv_writer.writerow(row)
+            CSV_WRITER_QUEUE.task_done()  # Be polite.
 
 
-async def get_row(semaphore, session, rank, domain):
+async def get_row(
+    semaphore: asyncio.Semaphore,
+    session: aiohttp.ClientSession,
+    rank: str,
+    domain: str,
+):
+    """Coroutine/Task that puts successful attempts into the CSV_WRITER_QUEUE.
+
+    Args:
+        semaphore: asyncio.Semaphore. Lock used to control connection pool.
+        session: aiohttp.ClientSession. Session maintaining TCPConnector.
+        rank: String. The rank of the domain.
+        domain: String. The domain itself, which is promptly formatted into a
+            real url.
+
+    Side Effects:
+        Puts successful records into the queue.
+    """
     url = f"http://www.{domain}"
-    favicon_url = None
 
     # First, make a blind attempt.
     try:
         favicon_url = await _try_get_favicon(semaphore, session, url)
-        if favicon_url:
-            await CSV_WRITER_QUEUE.put((url, rank, favicon_url))
     except Exception as e:
-        pass  # We don't really care.
+        favicon_url = None
+
+    if favicon_url:  # Success.
+        await CSV_WRITER_QUEUE.put((url, rank, favicon_url))
 
     # Otherwise, we have to dive into the HTML.
     try:
         text_content = await _fetch_html(semaphore, session, url)
     except Exception as e:
-        counter[type(e)] += 1
-        counter["fail"] += 1
-        return None
+        logger.exception(e)
+        return None  # Exit early.
 
     if not text_content:
-        counter["no text"] += 1
-        counter["fail"] += 1
-        return None
+        logger.debug(f"No text content for {url}.")
+        return None  # Exit early.
 
     favicon_url = get_favicon(text_content, url)
 
     if not favicon_url:
-        counter["no favicon"] += 1
-        counter["fail"] += 1
-        return None
+        logger.debug(f"No favicon found for {url}.")
+        return None  # Exit early.
 
     await CSV_WRITER_QUEUE.put((url, rank, favicon_url))
 
 
-async def run(pairs):
-    """Main doer of work."""
-    # all_results = []
+async def consume(pairs: Iterable[Tuple[str, str]]):
+    """Consumes pairs of ranks and domains.
+
+    Args:
+        pairs: Iterable of tuples of rank and domain strings.
+
+    Side Effects:
+        Upon completion of the entire fleet of requests, puts a None/"term"
+        signal into the queue.
+    """
+
+    # TODO(rico): Figure out why these get finicky when put into global scope?
     semaphore = asyncio.Semaphore(1000)
-    one_thousand = aiohttp.TCPConnector(limit=300)
+    tcp_connector = aiohttp.TCPConnector(limit=300)
 
     async with aiohttp.ClientSession(
-        loop=EVENT_LOOP, connector=one_thousand
+        loop=EVENT_LOOP, connector=tcp_connector
     ) as session:
         rows = await asyncio.gather(
             *[get_row(semaphore, session, *pair) for pair in pairs]
         )
 
-        successes = [row for row in rows if row is not None]
+        # successes = [row for row in rows if row is not None]
 
     await CSV_WRITER_QUEUE.put(None)
+    CSV_WRITER_QUEUE.task_done()
 
     return 0
 
 
-def main(csv_file_path=DEFAULT_CSV, total_number=TWO_HUNDRED_THOUSAND):
-    """Main execution context, controls event loop."""
+def main(
+    csv_file_path: str = DEFAULT_CSV, total_number: int = TWO_HUNDRED_THOUSAND
+):
+    """Main execution context, controls event loop.
+
+    Args:
+        csv_file_path: String. The file path to write to.
+        total_number: Integer. The total number of domains to process.
+
+    Side Effects:
+        Runs a loop.
+    """
     # 1) Set up CSV file.
     csv_file = open(csv_file_path, "w", 1000)
     csv_writer = csv.writer(csv_file)
@@ -223,11 +291,14 @@ def main(csv_file_path=DEFAULT_CSV, total_number=TWO_HUNDRED_THOUSAND):
     # 2) Get work chunked up,
     pairs = get_ranks_and_domains(total_number)
 
+    # 3) Run Event Loop.
     try:
         EVENT_LOOP.create_task(write_to_csv(csv_writer))
-        EVENT_LOOP.run_until_complete(run(pairs))
-    except Exception as e:  # this is pretty gross but whatever
-        print(f"******FATAL ERROR********")
+        EVENT_LOOP.run_until_complete(consume(pairs))
+    except Exception as e:
+        # Note: I would never do bare exception handling in a real application,
+        # This is just so we can see what this quick-and-dirty script does.
+        logger.error(f"***FATAL ERROR***: {e}")
         asyncio.gather(*asyncio.Task.all_tasks()).cancel()
     finally:
         # Shutting down and closing file descriptors after interrupt
